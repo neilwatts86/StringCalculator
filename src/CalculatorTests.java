9ea5b896745970b1e126d498b32af3f34b7f707a
Neil <neilwatts86@gmail.com>
2022-10-10 15:51:42 +0100

    First Test implemented, not green

--- src/CalculatorTests.java
+++ src/CalculatorTests.java
@@ -1,15 +1,22 @@
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTests {
 
+    Calculator systemUnderTest;
+
+    void setUp() {
+        systemUnderTest = new Calculator();
+    }
     @Test
     void AnEmptyStringReturnsZero() {
 
-        // Arrange
-        Calculator systemUnderTest = new Calculator();
-
+        //Arrange
+        setUp();
         // Act
         int response = systemUnderTest.Add("");
 
@@ -19,8 +26,9 @@ public class CalculatorTests {
 
     @Test
     void ASingleNumberReturnsItself() {
-        // Arrange
-        Calculator systemUnderTest = new Calculator();
+
+        //Arrange
+        setUp();
 
         // Act
         int response = systemUnderTest.Add("1");
@@ -29,15 +37,51 @@ public class CalculatorTests {
         assertEquals(response, 1);
     }
 
-    @Test
-    void twoNumbersReturnSum() {
-        // Arrange
-        Calculator systemUnderTest = new Calculator();
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2',3",
+            "'1,3',4",
+            "'12,6',18"})
+    void twoNumbersReturnSum(String csv, int expectedResponse) {
+
+        //Arrange
+        setUp();
 
         // Act
-        int response = systemUnderTest.Add("1,2");
+        int response = systemUnderTest.Add(csv);
 
         // Assert
-        assertEquals(response, 3);
+        assertEquals(response, expectedResponse);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2,3',6",
+            "'1,2,10',13",
+            "'50,150,100',300"
+    })
+    void AnyAmountOfNumbersReturnSum(String csv, int expectedResponse) {
+
+        //Arrange
+        setUp();
+
+        // Act
+        int response = systemUnderTest.Add(csv);
+
+        // Assert
+        assertEquals(response, expectedResponse);
+    }
+
+    @Test
+    void CsvAndNewLineStringReturnsSum() {
+
+        //Arrange
+        setUp();
+
+        // Act
+        int response = systemUnderTest.Add("1\n2,3");
+
+        // Assert
+        assertEquals(response, 6);
     }
 }
